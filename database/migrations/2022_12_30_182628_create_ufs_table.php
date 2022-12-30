da372eef63666ec6c2b3d645a066690be46b3929
Rodrigo Castro <rodrigocastro101998@gmail.com>
2022-12-30 15:33:48 -0300

    Crea mcr de uf

---
+++ database/migrations/2022_12_30_182628_create_ufs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ufs', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('nombreIndicador');
+            $table->string('codigoIndicador');
+            $table->string('unidadMedidaIndicador');
+            $table->string('valorIndicador');
+            $table->string('fechaIndicador');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ufs');
+    }
+};
