216a0a913988cc2e49f79a76530449386d29d60e
Rodrigo Castro <rodrigocastro101998@gmail.com>
2022-12-30 17:07:58 -0300

    Corrige nombre del archivo que carga la base de datos

--- jsonp.php
+++ jsonp.php
@@ -6,7 +6,7 @@ $data = file_get_contents($filename);
 $array = json_decode($data, true);
 foreach($array as $row)
 {
-    $sql = "INSERT INTO uf(id,nombreIndicador,codigoIndicador,unidadMedidaIndicador,valorIndicador,fechaIndicador)
+    $sql = "INSERT INTO ufs(id,nombreIndicador,codigoIndicador,unidadMedidaIndicador,valorIndicador,fechaIndicador)
     VALUES('".$row["id"]."',
     '".$row["nombreIndicador"]."',
     '".$row["codigoIndicador"]."',
